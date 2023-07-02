40fe6b77cb38ae8642b7d3226541a012e408b871
AmanRawat135 <87294884+AmanRawat135@users.noreply.github.com>
2023-07-02 20:09:30 +0530

    Update server.js
    code added for deployment

--- server.js
+++ server.js
@@ -23,16 +23,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, './client/build')));
 
 //routes
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/blog", blogRoutes);
 
-//rest api
-app.use('*', function(req, res){
-  res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
+//static files
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/indexedDB.html"));
+});
 
 // Port
 const PORT = process.env.PORT || 8080;
